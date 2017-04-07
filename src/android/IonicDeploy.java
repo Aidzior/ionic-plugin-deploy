6433cb01ae47be6c4ea078a18001dcc773f58508
Andrzej Przedecki <andrzej.przedecki@appyourself.net>
2017-04-07 11:03:45 +0200

    Update IonicDeploy.java

--- src/android/IonicDeploy.java
+++ src/android/IonicDeploy.java
@@ -712,6 +712,7 @@ public class IonicDeploy extends CordovaPlugin {
     } catch(Exception e) {
       //TODO Handle problems..
       logMessage("UNZIP_STEP", "Exception: " + e.getMessage());
+      logMessage("UNZIP_STEP", "Stacktrace: " + Log.getStackTraceString(e))
 
       // clean up any zip files dowloaded as they may be corrupted, we can download again if we start over
       String wwwFile = this.myContext.getFileStreamPath(zip).getAbsolutePath().toString();
