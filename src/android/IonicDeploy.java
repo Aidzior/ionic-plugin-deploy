a181e34a9544d91b7d69d6d475944b64b65b4a99
Aidzior <andrzej.idzior@gmail.com>
2021-08-01 18:31:19 +0200

    Update IonicDeploy.java

--- src/android/IonicDeploy.java
+++ src/android/IonicDeploy.java
@@ -802,8 +802,8 @@ public class IonicDeploy extends CordovaPlugin {
               settings.setAllowFileAccessFromFileURLs(true);
               logMessage("REDIRECT", "Loading deploy version: " + uuid);
               prefs.edit().putString("loaded_uuid", uuid).apply();
-              webView.loadUrlIntoView(indexLocation, false);
-              webView.clearHistory();
+              //webView.loadUrlIntoView(indexLocation, false);
+              //webView.clearHistory();
             }
             catch(Exception e) {
                       Log.i("my custom error:",  Log.getStackTraceString(e));
